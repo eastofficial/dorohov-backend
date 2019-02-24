d97f8651e557d19f76f697d84265d40c37640d58
Dmitrii <androsov.dima2013@yandex.ru>
2019-02-25 01:23:00 +0300

    autoload

--- index.php
+++ index.php
@@ -16,8 +16,7 @@ require_once(ROOT.'/components/Router.php');
 // Установка соединения с БД
 
 // define('ROOT', dirname(__FILE__));
-require_once(ROOT.'/components/Router.php');
-require_once(ROOT.'/components/Db.php');
+require_once(ROOT.'/components/Autoload.php');
 
 // Вызов Router
 
