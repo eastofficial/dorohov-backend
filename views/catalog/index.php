7cc07729883ecd888431d9a759a7d29b40d5e32a
Dmitrii <androsov.dima2013@yandex.ru>
2019-02-25 03:36:23 +0300

    release

--- views/catalog/index.php
+++ views/catalog/index.php
@@ -24,21 +24,25 @@
 
             <div class="col-sm-9 padding-right">
                 <div class="features_items"><!--features_items-->
-                    <h2 class="title text-center">Товары</h2>
+                    <h2 class="title text-center">Последние товары</h2>
                     
                     <?php foreach ($latestProducts as $product): ?>
                         <div class="col-sm-4">
                             <div class="product-image-wrapper">
                                 <div class="single-products">
                                     <div class="productinfo text-center">
-                                        <img src="/template/images/home/product1.jpg" alt="" />
-                                        <h2><?php echo $product['price'];?>$</h2>
+                                        <img src="<?php echo Product::getImage($product['id']); ?>" alt="" />
+                                        <h2>$<?php echo $product['price'];?></h2>
                                         <p>
                                             <a href="/product/<?php echo $product['id'];?>">
                                                 <?php echo $product['name'];?>
                                             </a>
                                         </p>
-                                        <a href="#" class="btn btn-default add-to-cart"><i class="fa fa-shopping-cart"></i>В корзину</a>
+                                        
+                                        <a href="#" data-id="<?php echo $product['id'];?>"
+                                           class="btn btn-default add-to-cart">
+                                            <i class="fa fa-shopping-cart"></i>В корзину
+                                        </a>
                                     </div>
                                     <?php if ($product['is_new']): ?>
                                         <img src="/template/images/home/new.png" class="new" alt="" />
